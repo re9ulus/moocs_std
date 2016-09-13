9a908f902b8c8d1b24582b7748668b42803f58c4
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-09-13 19:58:02 +0300

    Add task 3 solution

--- stepic_stat_2/w1/w1_tasks.r
+++ stepic_stat_2/w1/w1_tasks.r
@@ -1,9 +1,25 @@
-# Task 2 
+# Task 2, Done
 NA_position <- function(x, y) {
   all(is.na(x) == is.na(y))
 }
 
-# Task 3
+# Task 3, Done
 smart_test <- function(df) {
-  
+  t <- table(df)
+  if (all(t >= 5)) {
+    test.result <- chisq.test(t)
+    result <- c(test.result$statistic,
+                test.result$parameter,
+                test.result$p.value)
+  } else {
+    result <- fisher.test(t)$p.value
+  }
+  result
 }
+
+# smart_test(mtcars[1:20, c("am", "vs")])
+# 0.628483
+
+# smart_test(mtcars[,c("am", "vs")])
+# 0.3475355 1.0000000 0.5555115 
+
