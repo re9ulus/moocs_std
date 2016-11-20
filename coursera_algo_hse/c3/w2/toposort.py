4b02d6a1df8245a6ddb3d667c55aba13929cca67
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-11-20 19:58:27 +0300

    Update algo course, add toposort

--- coursera_algo_hse/c3/w2/toposort.py
+++ coursera_algo_hse/c3/w2/toposort.py
@@ -1,17 +1,28 @@
-#Uses python3
+#Uses python2
 
 import sys
 
 def dfs(adj, used, order, x):
     #write your code here
-    pass
+    global time
+    if used[x]:
+        return
+    used[x] = True
+    for to in adj[x]:
+        dfs(adj, used, order, to)
+    time = time + 1
+    order.append((time, x))
 
 
 def toposort(adj):
-    used = [0] * len(adj)
+    used = [False] * len(adj)
     order = []
     #write your code here
+    for i in range(len(adj)):
+        dfs(adj, used, order, i)
+    order = [y + 1 for x, y in sorted(order, reverse=True)]
     return order
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -20,9 +31,11 @@ if __name__ == '__main__':
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     adj = [[] for _ in range(n)]
+    time = 0
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
     order = toposort(adj)
-    for x in order:
-        print(x + 1, end=' ')
+    print(' '.join(map(str ,order)))
+    # for x in order:
+        # print(x + 1, end=' ')
 
