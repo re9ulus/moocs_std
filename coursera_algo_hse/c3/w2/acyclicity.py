d1105a823c637b01d8e1580caf86500f9f5b86b1
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-11-20 19:44:27 +0300

    Update algo course 3

--- coursera_algo_hse/c3/w2/acyclicity.py
+++ coursera_algo_hse/c3/w2/acyclicity.py
@@ -2,9 +2,23 @@
 
 import sys
 
+def dfs(adj, node):
+    if color[node] == 2:
+        return False
+    elif color[node] == 1:
+        return True
+    color[node] = 1
+    res = False
+    for to in adj[node]:
+        res |= dfs(adj, to)
+    color[node] = 2
+    return res
 
 def acyclic(adj):
-    return 0
+    res = False
+    for i in range(n):
+        res |= dfs(adj, i)
+    return 1 if res else 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -15,4 +29,6 @@ if __name__ == '__main__':
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
+
+    color = [0 for i in range(n)]
     print(acyclic(adj))
